c58be2a4b8982a871d10bec3b8dba0da997302db
LifeOfBrianOC <LifeOfBrianOC@users.noreply.github.com>
2015-01-29 11:10:06 +0000

    Script to add multiple hosts to vCenter

---
+++ add-multiple-hosts-to-vcenter.ps1
@@ -0,0 +1,5 @@
+# This script will add multiple esxi hosts hased on their names
+# In the example below it will add esx218.lab.local to esx225.lab.local
+
+# Add multiple hosts
+218..225 | Foreach-Object { Add-VMHost esx$_.lab.local -Location (Get-Datacenter PPBlack) -User vcenterAdminUserName -Password vcenterAdminUserPassword -RunAsync -force:$true}
